30ef522e8f83ce0f6485088eedca4f0828f67c45
Justyna Skalska <justyna_skalska@o2.pl>
2019-03-01 20:01:05 +0100

    Add random search class

---
+++ src/main/java/org/jastka4/knapsack/RandomSearch.java
@@ -0,0 +1,20 @@
+package org.jastka4.knapsack;
+
+public class RandomSearch implements KnapsackAlgorithm {
+
+	private ProblemInstance problemInstance;
+
+	public RandomSearch(final ProblemInstance problemInstance) {
+		this.problemInstance = problemInstance;
+	}
+
+	@Override
+	public Solution solve() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+}
